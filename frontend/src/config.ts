b62cb2c6ceaec62fb11ea6392e1d572010405c1d
Walter Barucco <walter.barucco3105@gmail.com>
2026-02-17 13:21:18 +0100

    Configurazione URL per il deploy

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,1 @@
-// Se siamo in sviluppo (localhost), usa la porta 8080.
-// Se siamo online (Vercel), useremo l'indirizzo del backend di Render.
 export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
